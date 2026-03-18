a183d9252aabcc00c5d0cc4d788bbf44aa76fe89
Yuvnish017 <malhotrayuvnish01@gmail.com>
2026-03-18 23:15:21 +0900

    adding auth middleware for jwt token verification

---
+++ internal/auth/middleware.go
@@ -0,0 +1,63 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	jwt "github.com/golang-jwt/jwt/v5"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Claims struct {
+	UserId uint `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
+
+func ValidateToken(tokenString string, secret string) (uint, error) {
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (any, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Method.Alg())
+		}
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	if !token.Valid {
+		return 0, fmt.Errorf("invalid token")
+	}
+
+	return claims.UserId, nil
+}
+
+func JwtAuthMiddleware(secret string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.Request.Header.Get("Authorization")
+		t := strings.Split(authHeader, " ")
+		if len(t) == 2 {
+			authToken := t[1]
+			userID, err := ValidateToken(authToken, secret)
+			if err != nil {
+				c.JSON(http.StatusUnauthorized, ErrorResponse{Message: err.Error()})
+				c.Abort()
+				return
+			}
+
+			c.Set("x-user-id", userID)
+			c.Next()
+			return
+		}
+		c.JSON(http.StatusUnauthorized, ErrorResponse{Message: "Not authorized"})
+		c.Abort()
+	}
+}
---
+++ internal/auth/model.go
@@ -0,0 +1,17 @@
+package auth
+
+type SignUpRequest struct {
+	Name            string `form:"name" binding:"required"`
+	Email           string `form:"email" binding:"required,email"`
+	Password        string `form:"password" binding:"required"`
+	ConfirmPassword string `form:"confirmPassword" binding:"required"`
+}
+
+type LoginRequest struct {
+	Email    string `form:"email" binding:"required,email"`
+	Password string `form:"password" binding:"required"`
+}
+
+type LoginResponse struct {
+	AccessToken string `json:"accessToken"`
+}
