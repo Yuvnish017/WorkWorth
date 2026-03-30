93fc9c466804718c786ed46c5129b2103ac1d4e8
Yuvnish017 <malhotrayuvnish01@gmail.com>
2026-03-30 20:16:14 +0900

    adding user repo

--- internal/users/model.go
+++ internal/users/model.go
@@ -1,12 +1,21 @@
 package users
 
 import (
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
-	ID       primitive.ObjectID `bson:"_id"`
-	Name     string             `bson:"name"`
-	Email    string             `bson:"email"`
-	Password string             `bson:"password"`
+	ID        primitive.ObjectID `bson:"_id"`
+	Name      string             `bson:"name"`
+	Email     string             `bson:"email"`
+	Password  string             `bson:"password"`
+	CreatedAt time.Time          `bson:"created_at"`
+}
+
+type UserRepository interface {
+	Create(name, email, password string) (*User, error)
+	GetUserByEmail(email string) (*User, error)
+	GetUserByID(id primitive.ObjectID) (*User, error)
 }
---
+++ internal/users/repository.go
@@ -0,0 +1,83 @@
+package users
+
+import (
+	"database/sql"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type userRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *userRepository {
+	return &userRepository{
+		db: db,
+	}
+}
+
+func (ur *userRepository) Create(name, email, password string) (*User, error) {
+	query := `
+		INSERT INTO users (name, email, password)
+		VALUES ($1, $2, $3)
+		RETURNING _id, name, email, password, created_at
+	`
+
+	var user User
+
+	err := ur.db.QueryRow(query, name, email, password).Scan(
+		&user.ID,
+		&user.Name,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (ur *userRepository) GetUserByEmail(email string) (*User, error) {
+	query := `
+		SELECT _id, name, email, password, created_at
+		FROM users
+		WHERE email = $1
+	`
+
+	var user User
+	err := ur.db.QueryRow(query, email).Scan(
+		&user.ID,
+		&user.Name,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (ur *userRepository) GetUserByID(id primitive.ObjectID) (*User, error) {
+	query := `
+		SELECT _id, name, email, password, created_at
+		FROM users
+		WHERE _id = $1
+	`
+
+	var user User
+	err := ur.db.QueryRow(query, id).Scan(
+		&user.ID,
+		&user.Name,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
